0ed1c148498c0211fe4fc2902f1bfbae9120028b
MarionBaussart <marion.baussart@gmail.com>
2022-04-19 11:37:01 +0000

    T0, puppet lint

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # create a file in /tmp
 file { 'create file':
-    path => '/tmp/school',
-    mode => '0744',
-    owner => 'www-data',
-    group => 'www-data',
+    path    => '/tmp/school',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
     content => 'I love Puppet',
 }
