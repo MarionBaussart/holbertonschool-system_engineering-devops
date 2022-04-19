92abb56e0cf5b9842020f2e1037e869893a470ef
MarionBaussart <marion.baussart@gmail.com>
2022-04-19 12:19:44 +0000

    T2, puppet lint

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # create a manifest that kills a process named killmenow
 exec { 'kills a process named killmenow':
-    command  => 'pkill killmenow',
-    path     => '/usr/bin:/usr/sbin:/bin',
+    command => 'pkill killmenow',
+    path    => '/usr/bin:/usr/sbin:/bin',
 }
