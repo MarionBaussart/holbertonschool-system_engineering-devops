a21537fcdb94bb213a6a5d89005c32997383e09d
MarionBaussart <marion.baussart@gmail.com>
2022-04-19 12:17:04 +0000

    T1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # install puppet-lint
 exec { 'puppet-lint':
-    command => 'sudo gem install puppet-lint',
-    path    => '/usr/bin',
+    command => 'sudo gem install puppet-lint -v 2.5.0',
+    path    => ['/usr/bin'],
 }
