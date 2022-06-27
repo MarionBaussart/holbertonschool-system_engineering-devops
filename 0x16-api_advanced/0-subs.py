63cee64c5a2db17624d2e83b437ce6917eefb6a5
MarionBaussart <marion.baussart@gmail.com>
2022-06-27 19:55:53 +0000

    T0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" queries the Reddit API and returns the number of subscribers """
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ function that returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit"""
+
+    response = requests.get('https://www.reddit.com/r/{}/about.json'.format(subreddit), headers = {'User-agent': 'Holberton'}, allow_redirects=False)
+
+    if response.status_code == 404:
+        return 0
+    return response.json()['data']['subscribers']
