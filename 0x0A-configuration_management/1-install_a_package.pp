752e2499d29135a26b621ef938f4bbd58366845a
MarionBaussart <marion.baussart@gmail.com>
2022-07-22 11:34:16 +0000

    install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# install puppet-lint
+# install flask
 exec { 'flask':
-    command => 'sudo gem install flask -v2.1.0',
+    command => 'sudo gem install flask -v 2.1.0',
     path    => ['/usr/bin'],
 }
